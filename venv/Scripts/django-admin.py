66090b561d3c0f9eb8a5ecd4d28ea93b5d3af882
Recep KARABACAK <ram.lantis@gmail.com>
2019-09-19 13:24:42 +0300

    start project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\programlama\python\projeler\web\score_board\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

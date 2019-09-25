d63fe0234d3479271949ef1c85aef1b1d00d1ffb
Recep KARABACAK <ram.lantis@gmail.com>
2019-09-25 16:36:26 +0300

    Settings-Allow_Host

--- Score_Board/settings.py
+++ Score_Board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p404fk(!tuwaq33v$*rvsvu_xe8t(41kb=@l_=$5h^qv-+rpn5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.35.230']
 
 
 # Application definition
